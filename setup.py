539d65d3eb84468e6a1039484361a23bfe357310
Mahesh Kammineni <maheshkammineni@gmail.com>
2024-08-03 23:26:22 +0530

    Updated the set up file and the requirements.txt file

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
 from setuptools import find_packages, setup
 from typing import List
-HYPEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->List[str]:
-    ''' 
-    this function return the list of requirements"
-    '''
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    """This function returns the list of requirements."""
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n", " ") for req in requirements]
+        requirements = [req.strip() for req in requirements]  # strip() to remove any whitespace including newlines
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
 
     return requirements
 
-
-
-
 setup(
-    name = "ml_project",
-    version = "0.0.1",
-    author= "mahesh",
+    name="ml_project",
+    version="0.0.1",
+    author="mahesh",
     author_email="maheshkammineni@gmail.com",
-    packages= find_packages(),
-    install_requires = get_requirements('requirements.txt')
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
+
