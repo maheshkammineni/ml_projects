e99a51ce86277ef0ede54eb2a5b2929ea63bad36
Mahesh Kammineni <maheshkammineni@gmail.com>
2024-08-06 13:48:05 +0530

    updated data_tranformation and utils files

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,14 @@
 import os
 import sys
-import dill
+import joblib
 from src.exception import CustomException
-from src.logger import logging
 
 def save_object(file_path, obj):
     try:
-        # Extract the directory path from the file path
         dir_path = os.path.dirname(file_path)
-        
-        # Create the directory if it doesn't exist
         os.makedirs(dir_path, exist_ok=True)
-        
-        # Open the file in binary write mode and save the object using dill
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
-    
+            joblib.dump(obj, file_obj)
     except Exception as e:
-        # Raise a CustomException if an error occurs
         raise CustomException(e, sys)
+
